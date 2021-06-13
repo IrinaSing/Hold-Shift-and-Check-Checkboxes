16c3a7dab67b3c06550df855d2dda7f2c51eecae
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-13 10:25:07 +0200

    removed const

--- src/listeners/listener.js
+++ src/listeners/listener.js
@@ -1,5 +1,3 @@
 import { handleCheck } from '../handlers/handle-check.js'
 
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
-
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
